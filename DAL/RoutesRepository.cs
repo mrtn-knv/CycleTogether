cbeb23f47ad47969ea717bde41c7a0a645b526cb
Martina Kuneva <martina.kuneva@gmail.com>
2019-05-16 16:55:47 +0300

    Add route model.

---
+++ DAL/RoutesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using DAL.Contracts;
+using DAL.Models;
+using System.Linq;
+
+namespace DAL
+{
+    public class RoutesRepository : Repository<Route>, IRouteRepository
+    {
+        public RoutesRepository()
+        {
+
+        }
+       
+    }
+}
