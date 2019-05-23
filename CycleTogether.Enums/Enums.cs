5799e51118849621df098472fe7dd61f8a431368
Martina Kuneva <martina.kuneva@gmail.com>
2019-05-23 18:04:32 +0300

    Add enums for terrain, type, difficulty, endurance. Add difficulty calculation.

---
+++ CycleTogether.Enums/Enums.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CycleTogether.Enums
+{
+    public enum Terrain
+    {
+        Ragged,
+        Flat,
+        Extreme
+    }
+
+    public enum TypeOfRoute
+    {
+        MountainBike,
+        Family,
+        Classic,
+        FreeForAll
+    }
+
+    public enum Difficulty
+    {
+        Easy,
+        Intermediate,
+        Pro
+    }
+    public enum Endurance
+    {
+        fourHours,
+        fourToSixHours,
+        moreThanSixHours
+    }
+}
---
+++ CycleTogether.Routes/DifficultyCalculator.cs
@@ -0,0 +1,98 @@
+﻿using CycleTogether.Enums;
+using System;
+using WebModels;
+
+namespace CycleTogether.Routes
+{
+    public class DifficultyCalculator
+    {
+        public DifficultyCalculator()
+        {
+
+        }
+
+        public void Set(RouteWeb route)
+        {
+            if (IsBeginner(route))
+            {
+                route.Difficulty = Difficulty.Easy;
+            }
+            if (IsIntermediate(route))
+            {
+                route.Difficulty = Difficulty.Intermediate;
+            }
+            if (IsPro(route))
+            {
+                route.Difficulty = Difficulty.Pro;
+            }
+        }
+
+        private bool IsPro(RouteWeb route)
+        {
+            if (route.Endurance == Endurance.moreThanSixHours &&
+                route.Terrain == Terrain.Ragged)
+            {
+                return true;
+            }
+            else if (route.Endurance == Endurance.fourToSixHours &&
+                     route.Terrain == Terrain.Extreme)
+            {
+                return true;
+            }
+            else if (route.Endurance == Endurance.moreThanSixHours &&
+                     route.Terrain == Terrain.Flat)
+            {
+                return true;
+            }
+            else if (route.Endurance == Endurance.moreThanSixHours &&
+                     route.Terrain == Terrain.Extreme)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsIntermediate(RouteWeb route)
+        {
+            if (route.Endurance == Endurance.fourToSixHours &&
+                route.Terrain == Terrain.Ragged)
+            {
+                return true;
+            }
+            else if (route.Endurance == Endurance.fourToSixHours &&
+                     route.Terrain == Terrain.Flat)
+            {
+                return true;
+            }
+            else if (route.Endurance == Endurance.fourHours &&
+                     route.Terrain == Terrain.Extreme)
+            {
+                return true;
+            }
+            else if (route.Endurance == Endurance.fourToSixHours &&
+                     route.Terrain == Terrain.Ragged)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsBeginner(RouteWeb route)
+        {
+
+            if (route.Endurance == Endurance.fourHours &&
+                   route.Terrain == Terrain.Flat)
+            {
+                return true;
+            }
+            else if (route.Terrain == Terrain.Ragged &&
+                     route.Endurance == Endurance.fourHours)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
