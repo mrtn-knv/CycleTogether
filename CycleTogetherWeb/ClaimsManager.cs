fa37a968cdb996f748e4e168f02510687d8f84ad
Martina Kuneva <martina.kuneva@gmail.com>
2019-05-30 18:46:34 +0300

    Refactor : Add http accessor to avoid repeating code, add GetId method.

--- CycleTogetherWeb/ClaimsManager.cs
+++ CycleTogetherWeb/ClaimsManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
 
@@ -6,10 +8,26 @@ namespace CycleTogetherWeb
 {
     public class ClaimsManager
     {
-        public string GetEmail(ClaimsIdentity identity)
-        {           
-            var claims = identity.Claims;
+        private readonly IHttpContextAccessor _accessor;
+        public ClaimsManager(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+        public string GetEmail()
+        {
+            var claims = GetUserClaims();
             return claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.Email)).Value;
+        }
+
+        public string GetId()
+        {
+            var claims = GetUserClaims();
+            return claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.NameIdentifier)).Value;
+        }
+        private IEnumerable<Claim> GetUserClaims()
+        {
+            var identity = _accessor.HttpContext.User.Identity as ClaimsIdentity;
+            return identity.Claims;
         }
     }
 }
