73b576204ab3f5c5c0c8950f475d957278641a6c
Martina Kuneva <martina.kuneva@gmail.com>
2019-06-07 18:30:47 +0300

    Put ClaimsRetriever class into separate project to avoid circular dependency.

---
+++ CycleTogether.Claims/ClaimsRetriever.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace CycleTogether.Claims
+{
+    public class ClaimsRetriever
+    {
+        private readonly IHttpContextAccessor _accessor;
+        public ClaimsRetriever(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+        public string Email()
+        {
+            var claims = GetUserClaims();
+            return claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.Email)).Value;
+        }
+
+        public string Id()
+        {
+            var claims = GetUserClaims();
+            return claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.NameIdentifier)).Value;
+        }
+        private IEnumerable<Claim> GetUserClaims()
+        {
+            var identity = _accessor.HttpContext.User.Identity as ClaimsIdentity;
+            return identity.Claims;
+        }
+    }
+}
